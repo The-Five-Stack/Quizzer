c98ea5c0c9f2108c2d7b59e1134d7f3f66cec3da
tripham <tri.pham@myy.haaga-helia.fi>
2026-04-08 18:45:30 +0300

    add home page

---
+++ src/main/java/quizzer/fivestack/project/controller/HomeController.java
@@ -0,0 +1,16 @@
+package quizzer.fivestack.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @ResponseBody
+    @GetMapping("/")
+    public String home() {
+        return "App is running";
+    }
+
+}
