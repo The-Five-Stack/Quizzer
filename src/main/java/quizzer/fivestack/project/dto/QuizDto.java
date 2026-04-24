4bd2a9d277906c5e8b3503ee9e58569e395398e9
qynwphuu <qynwphuu@gmail.com>
2026-04-24 11:15:04 +0300

    fix: add JsonProperty to make it input only

--- src/main/java/quizzer/fivestack/project/dto/QuizDto.java
+++ src/main/java/quizzer/fivestack/project/dto/QuizDto.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -39,6 +40,7 @@ public class QuizDto {
 
     private CategoryDto category;
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Long categoryId;
 
     public QuizDto() {
